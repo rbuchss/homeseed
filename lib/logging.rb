f3f5c22dc2b0be7b4ec3bfc0576655714a4d7c51
rbuchss <rbuchss@gmail.com>
2014-07-25 13:13:54 -0400

    adding global logger level support

--- lib/logging.rb
+++ lib/logging.rb
@@ -3,18 +3,24 @@ module Logging
     @logger ||= Logging.logger_for(self.class.name)
   end
 
-  # Use a hash class-ivar to cache a unique Logger per class:
-  @loggers = {}
+  @@global_level = Logger::INFO
+  @@loggers = {}
 
   class << self
     def logger_for(classname)
-      @loggers[classname] ||= configure_logger_for(classname)
+      @@loggers[classname] ||= configure_logger_for(classname)
     end
 
     def configure_logger_for(classname)
       logger = Logger.new(STDOUT)
+      logger.level = @@global_level
       logger.progname = classname
       logger
+    end
+
+    def global_level=(level)
+      @@global_level = level
+      @@loggers.each { |classname,logger| logger.level = level }
     end
   end
 end
